897176b1f1803054bca12f1840ac3f2980a10165
John Kraal <jpakraal@gmail.com>
2013-04-02 08:57:27 +0200

    basic class for openinghours

---
+++ happyhour/OpeningHours.h
@@ -0,0 +1,13 @@
+//
+//  OpeningHours.h
+//  happyhour
+//
+//  Created by John Kraal on 4/2/13.
+//  Copyright (c) 2013 fancyappes. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface OpeningHours : NSObject
+
+@end
